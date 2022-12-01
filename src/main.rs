9233df1bdba584c469adbf28648bed9396586ac1
Loy van Beek <loy.vanbeek@gmail.com>
2022-12-01 21:10:45 +0100

    Define structs to represent cards

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+#[derive(Debug)]
+enum Color {
+    Red,
+    Green,
+    Purple,
+}
+
+
+#[derive(Debug)]
+enum Count {
+    One,
+    Two,
+    Three,
+}
+
+#[derive(Debug)]
+enum Shading {
+    Open,
+    Solid,
+    Striped,
+}
+
+#[derive(Debug)]
+enum Shape {
+    Diamond,
+    Oval,
+    Squiggle,
+}
+
+#[derive(Debug)]
+struct Card {
+    color: Color,
+    count: Count,
+    shading: Shading,
+    shape: Shape,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let a = Card{
+        color: Color::Purple,
+        count: Count::One,
+        shading: Shading::Open,
+        shape: Shape::Diamond,
+    };
+
+    println!("Hello, Set: {:?}", a);
 }
