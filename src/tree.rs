0a20af614f2cce0e26f704d442013f0dae67dbc0
Loy van Beek <loy.vanbeek@gmail.com>
2023-02-27 21:03:44 +0100

    Busy setting up Tree datastructure, from https://doc.rust-lang.org/book/ch15-06-reference-cycles.html

--- src/tree.rs
+++ src/tree.rs
@@ -1,94 +1,116 @@
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
+use std::cell::RefCell;
 
+#[derive(Debug)]
 pub struct TreeNode<T> {
     pub value: T,
-    pub children: Vec<Box<TreeNode<T>>>,
-    pub parent: Option<Rc<TreeNode<T>>>
+    pub children: RefCell<Vec<Rc<TreeNode<T>>>>,
+    pub parent: RefCell<Weak<TreeNode<T>>>
 }
 
 impl<T> TreeNode<T> {
     pub fn new(
                 value: T,
-                children: Vec<Box<TreeNode<T>>>
+                children: RefCell<Vec<Rc<TreeNode<T>>>>
               ) -> Self {
         TreeNode {
             value,
             children,
-            parent: None,
-        }
-    }
-}
-
-pub struct Tree<T> {
-    root: Option<TreeNode<T>>
-}
-
-impl<T> Tree<T> {
-    pub fn new(root: Option<TreeNode<T>>) -> Self {
-        Tree {
-            root
+            parent: RefCell::new(Weak::new()),
         }
     }
 
-    pub fn iter(&self) -> PreorderIter<T> {
-        PreorderIter::new(self.root.as_ref())
-    }
-}
-
-pub struct PreorderIter<'a, T> {
-    stack: Vec<&'a TreeNode<T>>
-}
-
-impl<'a, T> PreorderIter<'a, T> {
-    pub fn new<'b: 'a>(root: Option<&'b TreeNode<T>>) -> Self {
-        if let Some(node) = root {
-            PreorderIter {
-                stack: vec![node]
-            }
-        } else {
-            PreorderIter {
-                stack: vec![]
-            }
+    pub fn level(&self) -> usize {
+        match self.parent.borrow().upgrade() {
+            Some(parent) => 1 + parent.level(),
+            None => 0
         }
     }
+
+    // pub fn add_child(&mut self, mut child: Box<TreeNode<T>>) -> () {
+    //     self.children.push(child);
+    //     let immutable_self = *self;
+    //     let weak = Weak::new(); //::from(immutable_self);
+    //     child.parent = Some(weak); //Weak::from(*self));
+    // }
 }
 
-impl<'a, T> Iterator for PreorderIter<'a, T> {
-  type Item = &'a TreeNode<T>;
-  fn next(&mut self) -> Option<Self::Item> {
-    if let Some(node) = self.stack.pop() {
-        let mut item_iter = node.children.iter().peekable();
-        if let Some(child) = item_iter.next() {
-            self.stack.push(&child)
-        }
-        return Some(node)
-    }
-    return None
-  }
-}
+// pub struct Tree<T> {
+//     root: Option<TreeNode<T>>
+// }
+
+// impl<T> Tree<T> {
+//     pub fn new(root: Option<TreeNode<T>>) -> Self {
+//         Tree {
+//             root
+//         }
+//     }
+
+//     pub fn iter(&self) -> PreorderIter<T> {
+//         PreorderIter::new(self.root.as_ref())
+//     }
+// }
+
+// pub struct PreorderIter<'a, T> {
+//     stack: Vec<&'a TreeNode<T>>
+// }
+
+// impl<'a, T> PreorderIter<'a, T> {
+//     pub fn new<'b: 'a>(root: Option<&'b TreeNode<T>>) -> Self {
+//         if let Some(node) = root {
+//             PreorderIter {
+//                 stack: vec![node]
+//             }
+//         } else {
+//             PreorderIter {
+//                 stack: vec![]
+//             }
+//         }
+//     }
+// }
+
+// impl<'a, T> Iterator for PreorderIter<'a, T> {
+//   type Item = &'a TreeNode<T>;
+//   fn next(&mut self) -> Option<Self::Item> {
+//     if let Some(node) = self.stack.pop() {
+//         let mut item_iter = node.children.borrow().iter().peekable();
+//         if let Some(child) = item_iter.next() {
+//             self.stack.push(&child)
+//         }
+//         return Some(node)
+//     }
+//     return None
+//   }
+// }
 
 #[cfg(test)]
 mod tests {
-    use super::{Tree, TreeNode};
+    use super::*;
 
     #[test]
     fn test_create_tree() {
-        let a = TreeNode::new(4, vec![]);
-        let b = TreeNode::new(5, vec![]);
-        let c = TreeNode::new(2, vec![Box::from(a), Box::from(b)]);
+        let a = Rc::new(TreeNode::new('a', RefCell::new(vec![])));
+        let b = Rc::new(TreeNode::new('b', RefCell::new(vec![])));
+        let c = Rc::new(TreeNode::new('c', RefCell::new(vec![Rc::clone(&a), Rc::clone(&b)])));
+        let d = Rc::new(TreeNode::new('d', RefCell::new(vec![])));
+        let e = Rc::new(TreeNode::new('e', RefCell::new(vec![Rc::clone(&c), Rc::clone(&d)])));
         
-        let d = TreeNode::new(3, vec![]);
-        let e = TreeNode::new(1, vec![Box::from(c), Box::from(d)]);
+        println!("{:?}", e);
+
+        assert_eq!(e.level(), 0); // Top level
+        assert_eq!(d.level(), 1); // Child of e
+        // assert_eq!(c.level(), 1); // Child of e
+        // assert_eq!(b.level(), 2); // Child of c
+        // assert_eq!(a.level(), 2); // Child of c
+        // let tree = Tree::new(Some(e));
         
-        let tree = Tree::new(Some(e));
+        // for _node in tree.iter() {
+        //     // _node.value *= 10;
+        // }
         
-        for _node in tree.iter() {
-            // _node.value *= 10;
-        }
-        
-        let mut iterator = tree.iter();
-        while let Some(node) = iterator.next() { // equivalent to the for loop construction
-            println!("{}", node.value)
-        }
+        // let mut iterator = tree.iter();
+        // while let Some(node) = iterator.next() { // equivalent to the for loop construction
+        //     println!("{}", node.value)
+        // }
     }
 }
