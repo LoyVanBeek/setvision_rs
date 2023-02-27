b51fc629f59d9ea2142ea62a2e57bec397629e1d
Loy van Beek <loy.vanbeek@gmail.com>
2023-02-27 22:03:22 +0100

    Add a new function to add a child to a TreeNode. Finally makes level() work too

--- src/tree.rs
+++ src/tree.rs
@@ -32,13 +32,19 @@ impl<T: Display> TreeNode<T> {
         }
     }
 
-    pub fn add_child(self: Rc<TreeNode<T>>, mut child: Rc<TreeNode<T>>) -> () {
-        self.children.borrow_mut().push(Rc::clone(&child));
-        // let immutable_self = *self;
-        // let weak = Weak::new(); //::from(immutable_self);
-        // child.parent = RefCell::new(Weak::clone(&self));
-        *child.parent.borrow_mut() = Rc::downgrade(&self);
-    }
+    // pub fn add_child(&self, mut child: Rc<TreeNode<T>>) -> () {
+    //     self.children.borrow_mut().push(Rc::clone(&child));
+    //     // let immutable_self = *self;
+    //     // let weak = Weak::new(); //::from(immutable_self);
+    //     // child.parent = RefCell::new(Weak::clone(&self));
+    //     let rc_self = Rc::new(*self);
+    //     *child.parent.borrow_mut() = Rc::downgrade(&rc_self);
+    // }
+}
+
+pub fn add_child<T: Display>(parent: &mut Rc<TreeNode<T>>, child: &mut Rc<TreeNode<T>>) -> () {
+    parent.children.borrow_mut().push(Rc::clone(child));
+    *child.parent.borrow_mut() = Rc::downgrade(parent);
 }
 
 
@@ -48,22 +54,24 @@ mod tests {
 
     #[test]
     fn test_create_tree() {
-        let a = Rc::new(TreeNode::new('a', RefCell::new(vec![])));
-        let b = Rc::new(TreeNode::new('b', RefCell::new(vec![])));
+        let mut a = Rc::new(TreeNode::new('a', RefCell::new(vec![])));
+        let mut b = Rc::new(TreeNode::new('b', RefCell::new(vec![])));
         // let c = Rc::new(TreeNode::new('c', RefCell::new(vec![Rc::clone(&a), Rc::clone(&b)])));
-        let c = Rc::new(TreeNode::new('c', RefCell::new(vec![])));
-        // c.add_child(a);
-        // c.add_child(b);
-        let d = Rc::new(TreeNode::new('d', RefCell::new(vec![])));
+        let mut c = Rc::new(TreeNode::new('c', RefCell::new(vec![])));
+        add_child(&mut c, &mut a);
+        add_child(&mut c, &mut b);
+        let mut d = Rc::new(TreeNode::new('d', RefCell::new(vec![])));
         // let e = Rc::new(TreeNode::new('e', RefCell::new(vec![Rc::clone(&c), Rc::clone(&d)])));
-        let e = Rc::new(TreeNode::new('e', RefCell::new(vec![])));
+        let mut e = Rc::new(TreeNode::new('e', RefCell::new(vec![])));
+        add_child(&mut e, &mut c);
+        add_child(&mut e, &mut d);
         
         println!("{:?}", e);
 
         assert_eq!(e.level(), 0); // Top level
         assert_eq!(d.level(), 1); // Child of e
-        // assert_eq!(c.level(), 1); // Child of e
-        // assert_eq!(b.level(), 2); // Child of c
-        // assert_eq!(a.level(), 2); // Child of c
+        assert_eq!(c.level(), 1); // Child of e
+        assert_eq!(b.level(), 2); // Child of c
+        assert_eq!(a.level(), 2); // Child of c
     }
 }
